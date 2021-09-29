814d7c953fbbf97343b4241d75604d4b25ff622c
Aldair André <aldairandre99@gmail.com>
2021-09-29 23:45:32 +0100

    Estudo de rotas com express

--- 4_express/app.js
+++ 4_express/app.js
@@ -2,26 +2,9 @@ const express = require('express');
 
 const app = express();
 
-//Rota e Parametros
-
-app.get("/",function(req, res){
-    res.send("Seja bem-vindo ao meu app")
+app.get('/',function(req,res){
+    res.send('Bem vindo a minha rota inicial')
 })
-
-app.get("/sobre",function(req,res){
-    res.send("Minha pagina sobre")
-})
-
-app.get("/blog",function(req,res){
-    res.send("Bem-vindo ao meu blog")
-})
-
-//Parametros em rotas
-
-app.get("/ola/:nome/:cargo",function(req,res){
-    res.send(req.params)
-})
-
 
 app.listen(8082,function(){
     console.log('Servidor rodando na url http://localhost:8082');
