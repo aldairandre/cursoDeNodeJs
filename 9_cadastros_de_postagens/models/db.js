2adda4bb9b1e8d0112bffedb890f19e0b08c1420
aldairandre <aldairandre99@gmail.com>
2021-10-14 15:07:35 +0100

    alterando nome da db

--- 9_cadastros_de_postagens/models/db.js
+++ 9_cadastros_de_postagens/models/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('teste','root','tuayaniIsabel1',{
+const sequelize = new Sequelize('postapp','root','tuayaniIsabel1',{
     host: 'localhost',
     dialect : 'mysql'
 })
