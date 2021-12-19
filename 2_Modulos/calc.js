86dc023b550ce63cbda119b6cf268423b26f9725
Aldair André <aldairandre99@gmail.com>
2021-12-19 18:12:55 +0100

    calculadora basica

---
+++ 2_Modulos/calc.js
@@ -0,0 +1,9 @@
+const soma = require("./soma")
+const sub = require("./sub")
+const mult = require("./mult")
+const div = require("./div")
+
+console.log(soma(3,2))
+console.log(sub(3,2))
+console.log(mult(3,2))
+console.log(div(2,2))
